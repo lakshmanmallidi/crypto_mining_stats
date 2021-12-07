975dd975ff73b60f02b3eebdf7712efdfd223205
lakshman.mallidi <lakshman.mallidi@gmail.com>
2021-12-08 03:06:35 +0530

    last db push time changes.

--- cloud_vm/avg_stats.py
+++ cloud_vm/avg_stats.py
@@ -13,6 +13,7 @@ host = config.get('mariadb', 'host')
 user = config.get('mariadb', 'user')
 passwd = config.get('mariadb', 'passwd')
 database = config.get('mariadb', 'database')
+database_push_time = int(config.get('mariadb', 'db_push_wait'))
 stats_push_intervel = int(config.get('avg_stats', 'push_intervel'))
 stats_window = int(config.get('avg_stats', 'avg_window'))
 mqtt_host = config.get('mqtt_broker', 'host')
@@ -37,35 +38,48 @@ class stats(peewee.Model):
         db_table = 'stats'
 
 
-def pushAvgStats():
-    global stats_push_intervel, stats_window
+def pushLastDbInsertTime():
+    global database_push_time
     prev_time = time()
     while True:
-        try:
-            if(time()-prev_time > stats_push_intervel):
+        if(time()-prev_time > database_push_time):
+            try:
+                last_insertion_date = stats.select(
+                    peewee.fn.MAX(stats.stat_datetime)).scalar()
+                client.publish("stats/last_insert_at",
+                               payload=str(last_insertion_date), retain=True)
+            except Exception as e:
+                print("pushLastDbInsertTime:|:"+str(e))
+            prev_time = time()
+            sleep(database_push_time-5)
+
+
+def pushAvgStats():
+    global stats_push_intervel, stats_window, database_push_time
+    prev_time = time()
+    while True:
+        if(time()-prev_time > stats_push_intervel):
+            try:
                 windowed = (datetime.now() - timedelta(minutes=stats_window)) \
                     .replace(second=0, microsecond=0)
                 avg_voltage = stats.select().where(stats.stat_datetime > windowed) \
                     .select(peewee.fn.AVG(stats.voltage)).scalar()
                 avg_current = stats.select().where(stats.stat_datetime > windowed) \
                     .select(peewee.fn.AVG(stats.current)).scalar()
-                last_insertion_date = stats.select(
-                    peewee.fn.MAX(stats.stat_datetime)).scalar()
                 client.publish("stats/avg_voltage",
                                payload=round(avg_voltage, 2), retain=True)
                 client.publish("stats/avg_current",
                                payload=round(avg_current, 2), retain=True)
-                client.publish("stats/last_insert_at",
-                               payload=str(last_insertion_date), retain=True)
-                prev_time = time()
-                sleep(stats_push_intervel-5)
-        except Exception as e:
-            print(e)
+            except Exception as e:
+                print(e)
+            prev_time = time()
+            sleep(stats_push_intervel-5)
 
 
 def on_connect(client, userdata, flags, rc):
     if(rc == 0):
         Thread(target=pushAvgStats, args=()).start()
+        Thread(target=pushLastDbInsertTime, args=()).start()
     else:
         print("unable to connect to mqtt broker")
 
