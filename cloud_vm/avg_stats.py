2486ba89c056c964287696ca8a6f4c1bc96e5ee5
lakshman.mallidi <lakshman.mallidi@gmail.com>
2021-12-08 13:53:18 +0530

    avg stats loop to thread based approch.

--- cloud_vm/avg_stats.py
+++ cloud_vm/avg_stats.py
@@ -6,6 +6,7 @@ import paho.mqtt.client as mqtt
 from time import sleep, time
 import peewee
 from threading import Thread
+from timerThread import RepeatedTimer
 
 config = ConfigParser()
 config.read(path.join(path.dirname(path.abspath(__file__)), 'config.ini'))
@@ -39,52 +40,43 @@ class stats(peewee.Model):
 
 
 def pushLastDbInsertTime():
-    global database_push_time
-    prev_time = time()
-    while True:
-        if(time()-prev_time > database_push_time):
-            try:
-                last_insertion_date = stats.select(
-                    peewee.fn.MAX(stats.stat_datetime)).scalar()
-                client.publish("stats/last_insert_at",
-                               payload=str(last_insertion_date), retain=True)
-            except Exception as e:
-                print("pushLastDbInsertTime:|:"+str(e))
-            prev_time = time()
-            sleep(database_push_time-5)
+    try:
+        last_insertion_date = stats.select(
+            peewee.fn.MAX(stats.stat_datetime)).scalar()
+        client.publish("stats/last_insert_at",
+                       payload=str(last_insertion_date), retain=True)
+    except Exception as e:
+        print("pushLastDbInsertTime:|:"+str(e))
 
 
 def pushAvgStats():
-    global stats_push_intervel, stats_window, database_push_time
-    prev_time = time()
-    while True:
-        if(time()-prev_time > stats_push_intervel):
-            try:
-                windowed = (datetime.now() - timedelta(minutes=stats_window)) \
-                    .replace(second=0, microsecond=0)
-                avg_voltage = stats.select().where(stats.stat_datetime > windowed) \
-                    .select(peewee.fn.AVG(stats.voltage)).scalar()
-                avg_current = stats.select().where(stats.stat_datetime > windowed) \
-                    .select(peewee.fn.AVG(stats.current)).scalar()
-                client.publish("stats/avg_voltage",
-                               payload=round(avg_voltage, 2), retain=True)
-                client.publish("stats/avg_current",
-                               payload=round(avg_current, 2), retain=True)
-            except Exception as e:
-                print(e)
-            prev_time = time()
-            sleep(stats_push_intervel-5)
+    try:
+        windowed = (datetime.now() - timedelta(minutes=stats_window)) \
+            .replace(second=0, microsecond=0)
+        avg_voltage = stats.select().where(stats.stat_datetime > windowed) \
+            .select(peewee.fn.AVG(stats.voltage)).scalar()
+        avg_current = stats.select().where(stats.stat_datetime > windowed) \
+            .select(peewee.fn.AVG(stats.current)).scalar()
+        client.publish("stats/avg_voltage",
+                       payload=round(avg_voltage, 2), retain=True)
+        client.publish("stats/avg_current",
+                       payload=round(avg_current, 2), retain=True)
+    except Exception as e:
+        print(e)
 
 
 def on_connect(client, userdata, flags, rc):
     if(rc == 0):
-        Thread(target=pushAvgStats, args=()).start()
-        Thread(target=pushLastDbInsertTime, args=()).start()
+        avg_stats_pusher.start()
+        last_insertion_pusher.start()
     else:
         print("unable to connect to mqtt broker")
 
 
 try:
+    avg_stats_pusher = RepeatedTimer(stats_push_intervel, pushAvgStats)
+    last_insertion_pusher = RepeatedTimer(
+        database_push_time, pushLastDbInsertTime)
     client = mqtt.Client(client_user)
     client.username_pw_set(mqtt_user, mqtt_passwd)
     client.on_connect = on_connect
